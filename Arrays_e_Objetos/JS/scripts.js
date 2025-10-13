a381b5fde7eddb1185b54333a298f9a54f1cc04c
Johnny Matheus <alunotioficial@gmail.com>
2025-10-12 23:43:15 -0300

    Update scripts.js

--- Arrays_e_Objetos/JS/scripts.js
+++ Arrays_e_Objetos/JS/scripts.js
@@ -10,5 +10,37 @@ lista2 = ['Johnny Matheus', 30, 1.85];
 
 console.log(lista2);
 
+// 2 - indices
 
-// 
+const alfabeto =['a','b','c','d','e'];
+console.log(alfabeto[0]);
+console.log(alfabeto[4]);
+console.log(alfabeto[1]);
+
+// 3 - propriedades
+const numbers = [5,3,4];
+
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = 'Johnny';
+
+console.log(myName.length);
+
+// 4 - Métodos 
+
+const otherNumbers =[1,2,3];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = "Ciencia da Computação";
+
+console.log(text.toUpperCase());
+
+console.log(typeof text.toUpperCase);
+
+console.log(text.indexOf("C"))
+
